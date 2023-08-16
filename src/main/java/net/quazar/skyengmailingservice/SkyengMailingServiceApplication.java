08907a46b4583b985a892bed50d2530f533fccf0
SQuazar <flawesv@yandex.ru>
2023-08-16 20:02:59 +0300

    Initial commit

---
+++ src/main/java/net/quazar/skyengmailingservice/SkyengMailingServiceApplication.java
@@ -0,0 +1,13 @@
+package net.quazar.skyengmailingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyengMailingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyengMailingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/quazar/skyengmailingservice/SkyengMailingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.quazar.skyengmailingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyengMailingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
