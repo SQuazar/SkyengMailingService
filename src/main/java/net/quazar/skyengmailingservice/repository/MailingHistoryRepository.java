80b03ae7633cc3d68eecc104ab09a2f53ff5083b
SQuazar <flawesv@yandex.ru>
2023-08-18 19:37:06 +0300

    Added new method for search by mailing_id

--- src/main/java/net/quazar/skyengmailingservice/repository/MailingHistoryRepository.java
+++ src/main/java/net/quazar/skyengmailingservice/repository/MailingHistoryRepository.java
@@ -1,9 +1,13 @@
 package net.quazar.skyengmailingservice.repository;
 
+import net.quazar.skyengmailingservice.entity.Mailing;
 import net.quazar.skyengmailingservice.entity.MailingHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MailingHistoryRepository extends JpaRepository<MailingHistory, Integer> {
+    List<MailingHistory> findAllByMailing(Mailing mailing);
 }
