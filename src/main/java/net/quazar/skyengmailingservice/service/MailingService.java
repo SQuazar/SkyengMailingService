69e2376d1729923af8e72c5ce4c16d01529a085f
SQuazar <flawesv@yandex.ru>
2023-08-18 19:27:27 +0300

    Added method getMailingById

--- src/main/java/net/quazar/skyengmailingservice/service/MailingService.java
+++ src/main/java/net/quazar/skyengmailingservice/service/MailingService.java
@@ -8,6 +8,8 @@ import net.quazar.skyengmailingservice.entity.dto.MailingHistoryNodeDto;
 import java.util.Set;
 
 public interface MailingService {
+    MailingDto getMailingById(int mailingId);
+
     MailingDto registerNewMailing(Mailing.Type type, String receiverIndex, String receiverAddress, String receiverName);
 
     void changeMailingStatus(int mailingId, MailingStatus.Status status);
