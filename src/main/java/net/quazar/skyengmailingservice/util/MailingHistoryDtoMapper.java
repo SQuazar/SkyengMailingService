20abce84c3ef01b6fd7384c0eb61b80af128d8d5
SQuazar <flawesv@yandex.ru>
2023-08-18 19:36:48 +0300

    Fix return type

--- src/main/java/net/quazar/skyengmailingservice/util/MailingHistoryDtoMapper.java
+++ src/main/java/net/quazar/skyengmailingservice/util/MailingHistoryDtoMapper.java
@@ -1,6 +1,7 @@
 package net.quazar.skyengmailingservice.util;
 
 import net.quazar.skyengmailingservice.entity.MailingHistory;
+import net.quazar.skyengmailingservice.entity.dto.MailingHistoryNodeDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -11,5 +12,5 @@ public interface MailingHistoryDtoMapper {
 
     @Mapping(target = "mailingId", source = "mailing.id")
     @Mapping(target = "postalOfficeId", source = "postalOffice.id")
-    MailingHistoryDtoMapper mailingHistoryToDto(MailingHistory mailingHistory);
+    MailingHistoryNodeDto mailingHistoryToDto(MailingHistory mailingHistory);
 }
