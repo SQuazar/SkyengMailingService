7b6a6ea3fbb4f1d675ed67e9b3f78beb656ccd35
SQuazar <flawesv@yandex.ru>
2023-08-20 16:23:46 +0300

    Added PostalOfficeServiceImpl

---
+++ src/main/java/net/quazar/skyengmailingservice/service/impl/PostalOfficeServiceImpl.java
@@ -0,0 +1,90 @@
+package net.quazar.skyengmailingservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.quazar.skyengmailingservice.entity.Mailing;
+import net.quazar.skyengmailingservice.entity.MailingHistory;
+import net.quazar.skyengmailingservice.entity.MailingStatus;
+import net.quazar.skyengmailingservice.entity.PostalOffice;
+import net.quazar.skyengmailingservice.entity.dto.PostalOfficeDto;
+import net.quazar.skyengmailingservice.exception.MailingNotFoundException;
+import net.quazar.skyengmailingservice.exception.PostalOfficeAlreadyExistsException;
+import net.quazar.skyengmailingservice.exception.PostalOfficeNotFoundException;
+import net.quazar.skyengmailingservice.repository.MailingHistoryRepository;
+import net.quazar.skyengmailingservice.repository.MailingRepository;
+import net.quazar.skyengmailingservice.repository.MailingStatusRepository;
+import net.quazar.skyengmailingservice.repository.PostalOfficeRepository;
+import net.quazar.skyengmailingservice.service.PostalOfficeService;
+import net.quazar.skyengmailingservice.util.PostalOfficeDtoMapper;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class PostalOfficeServiceImpl implements PostalOfficeService {
+    private final PostalOfficeRepository postalOfficeRepository;
+    private final MailingRepository mailingRepository;
+    private final MailingStatusRepository mailingStatusRepository;
+    private final MailingHistoryRepository mailingHistoryRepository;
+
+    private final PostalOfficeDtoMapper postalOfficeMapper = PostalOfficeDtoMapper.INSTANCE;
+
+    @Override
+    public PostalOfficeDto createPostalOffice(String index, String address, String name) {
+        if (postalOfficeRepository.findById(index).isPresent())
+            throw new PostalOfficeAlreadyExistsException(String.format("Почтовое отделение по индексу %s уже зарегистрировано", index));
+        return postalOfficeMapper.postalOfficeToDto(
+                postalOfficeRepository.save(PostalOffice.builder()
+                        .index(index)
+                        .address(address)
+                        .name(name)
+                        .build())
+        );
+    }
+
+    @Override
+    public void registerIncomingMailing(String postalOfficeIndex, int mailingId) {
+        PostalOffice postalOffice = postalOfficeRepository.findById(postalOfficeIndex)
+                .orElseThrow(() -> new PostalOfficeNotFoundException(String.format("Почтовое отделение по индексу %s не найдено", postalOfficeIndex)));
+        Mailing mailing = mailingRepository.findById(mailingId)
+                .orElseThrow(() -> new MailingNotFoundException(String.format("Почтовое отправление %d не найдено", mailingId)));
+
+        mailingStatusRepository.save(MailingStatus.builder()
+                .mailing(mailing)
+                .status(MailingStatus.Status.ROUTING)
+                .build());
+        MailingHistory mailingHistory = MailingHistory.builder()
+                .mailing(mailing)
+                .postalOffice(postalOffice)
+                .date(LocalDateTime.now())
+                .operation(String.format("Прибыло в почтовое отделение %s %s \"%s\"",
+                        postalOffice.getIndex(),
+                        postalOffice.getAddress(),
+                        postalOffice.getName()))
+                .build();
+        mailingHistoryRepository.save(mailingHistory);
+    }
+
+    @Override
+    public void registerOutgoingMailing(String postalOfficeIndex, int mailingId) {
+        PostalOffice postalOffice = postalOfficeRepository.findById(postalOfficeIndex)
+                .orElseThrow(() -> new PostalOfficeNotFoundException(String.format("Почтовое отделение по индексу %s не найдено", postalOfficeIndex)));
+        Mailing mailing = mailingRepository.findById(mailingId)
+                .orElseThrow(() -> new MailingNotFoundException(String.format("Почтовое отправление %d не найдено", mailingId)));
+
+        mailingStatusRepository.save(MailingStatus.builder()
+                .mailing(mailing)
+                .status(MailingStatus.Status.ROUTING)
+                .build());
+        MailingHistory mailingHistory = MailingHistory.builder()
+                .mailing(mailing)
+                .postalOffice(postalOffice)
+                .date(LocalDateTime.now())
+                .operation(String.format("Отправлено из почтового отделения %s %s \"%s\"",
+                        postalOffice.getIndex(),
+                        postalOffice.getAddress(),
+                        postalOffice.getName()))
+                .build();
+        mailingHistoryRepository.save(mailingHistory);
+    }
+}
