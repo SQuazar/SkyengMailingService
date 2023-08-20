fa6b3389a098ef2030a43deee88ee55dea42e09e
SQuazar <flawesv@yandex.ru>
2023-08-20 19:08:18 +0300

    Added method findByMailingId

--- src/main/java/net/quazar/skyengmailingservice/repository/MailingStatusRepository.java
+++ src/main/java/net/quazar/skyengmailingservice/repository/MailingStatusRepository.java
@@ -4,6 +4,9 @@ import net.quazar.skyengmailingservice.entity.MailingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MailingStatusRepository extends JpaRepository<MailingStatus, Integer> {
+    Optional<MailingStatus> findByMailingId(int mailingId);
 }
