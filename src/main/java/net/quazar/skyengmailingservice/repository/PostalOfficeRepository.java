1858bc6408a2c8d5d5552c4eb91b98fdf64ccc79
SQuazar <flawesv@yandex.ru>
2023-08-20 16:10:47 +0300

    Changed key to String type

--- src/main/java/net/quazar/skyengmailingservice/repository/PostalOfficeRepository.java
+++ src/main/java/net/quazar/skyengmailingservice/repository/PostalOfficeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostalOfficeRepository extends JpaRepository<PostalOffice, Integer> {
+public interface PostalOfficeRepository extends JpaRepository<PostalOffice, String> {
 }
