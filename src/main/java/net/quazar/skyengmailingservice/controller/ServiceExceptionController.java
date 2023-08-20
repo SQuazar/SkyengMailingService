7d79956206a65f6c41d8940c9470bf1e2e2360f4
SQuazar <flawesv@yandex.ru>
2023-08-20 19:08:58 +0300

    Fixed handleServiceException and added new method for handling MethodArgumentNotValidException

--- src/main/java/net/quazar/skyengmailingservice/controller/ServiceExceptionController.java
+++ src/main/java/net/quazar/skyengmailingservice/controller/ServiceExceptionController.java
@@ -4,10 +4,15 @@ package net.quazar.skyengmailingservice.controller;
 import lombok.Builder;
 import lombok.Data;
 import net.quazar.skyengmailingservice.exception.ServiceException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ServiceExceptionController {
@@ -16,13 +21,28 @@ public class ServiceExceptionController {
         ResponseStatus responseStatus;
         int code = 500;
         if ((responseStatus = e.getClass().getAnnotation(ResponseStatus.class)) != null)
-            code = responseStatus.code().value();
+            code = responseStatus.value().value();
         return ResponseEntity.status(code)
-                .body(
-                        ServerError.builder()
+                .body(ServerError.builder()
                                 .code(code)
                                 .message(e.getMessage())
                                 .build()
+                );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ServerError> handleNotValidException(MethodArgumentNotValidException e) {
+        String message = "Invalid request body:" + ' ' +
+                e.getBindingResult().getAllErrors()
+                        .stream()
+                        .map(ObjectError::getDefaultMessage)
+                        .collect(Collectors.joining(", "));
+        return ResponseEntity.status(400)
+                .body(ServerError.builder()
+                        .code(400)
+                        .message(message)
+                        .build()
                 );
     }
 
