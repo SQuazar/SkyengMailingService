b596f85bddcd506770ce667503a3aa5877879c82
SQuazar <flawesv@yandex.ru>
2023-08-20 16:33:56 +0300

    Added Sort to findByMailing method

--- src/main/java/net/quazar/skyengmailingservice/repository/MailingHistoryRepository.java
+++ src/main/java/net/quazar/skyengmailingservice/repository/MailingHistoryRepository.java
@@ -2,6 +2,7 @@ package net.quazar.skyengmailingservice.repository;
 
 import net.quazar.skyengmailingservice.entity.Mailing;
 import net.quazar.skyengmailingservice.entity.MailingHistory;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface MailingHistoryRepository extends JpaRepository<MailingHistory, Integer> {
-    List<MailingHistory> findAllByMailing(Mailing mailing);
+    List<MailingHistory> findAllByMailing(Mailing mailing, Sort sort);
 }
