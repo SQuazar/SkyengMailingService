911f2ec77304cb643f7d1ed71d89e575f2a50717
SQuazar <flawesv@yandex.ru>
2023-08-22 00:42:54 +0300

    Added SwaggerUI

--- src/main/java/net/quazar/skyengmailingservice/controller/MailingController.java
+++ src/main/java/net/quazar/skyengmailingservice/controller/MailingController.java
@@ -1,8 +1,13 @@
 package net.quazar.skyengmailingservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.quazar.skyengmailingservice.controller.model.RegisterMailingRequest;
+import net.quazar.skyengmailingservice.controller.model.ServerError;
 import net.quazar.skyengmailingservice.entity.MailingStatus;
 import net.quazar.skyengmailingservice.entity.dto.MailingDto;
 import net.quazar.skyengmailingservice.entity.dto.MailingHistoryNodeDto;
@@ -17,21 +22,63 @@ import java.util.List;
 public class MailingController {
     private final MailingService mailingService;
 
+    @Operation(
+            description = "Получает почтовое отправление по его ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = ServerError.class
+                                    )
+                            )
+                    )
+            }
+    )
     @GetMapping("/{mailingId}")
     public MailingDto getMailingById(@PathVariable int mailingId) {
         return mailingService.getMailingById(mailingId);
     }
 
+    @Operation(
+            description = "Получает историю почтового отправления по его ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = ServerError.class
+                                    )
+                            )
+                    )
+            }
+    )
     @GetMapping("/{mailingId}/history")
     public List<MailingHistoryNodeDto> getMailingHistory(@PathVariable int mailingId) {
         return mailingService.getMailingHistory(mailingId);
     }
 
+    @Operation(
+            description = "Регистрирует новое почтовое отправление"
+    )
     @PostMapping
     public MailingDto registerNewMailing(@RequestBody @Valid RegisterMailingRequest request) {
         return mailingService.registerNewMailing(request.getMailingType(), request.getReceiverIndex(), request.getReceiverAddress(), request.getReceiverName());
     }
 
+    @Operation(
+            description = "Устанавливает статус почтового отправления как \"Получено\"",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = ServerError.class
+                                    )
+                            )
+                    )
+            }
+    )
     @PostMapping("/{mailingId}/delivered")
     public MailingDto setDelivered(@PathVariable int mailingId) {
         mailingService.changeMailingStatus(mailingId, MailingStatus.Status.RECEIVED);
--- src/main/java/net/quazar/skyengmailingservice/controller/PostalOfficeController.java
+++ src/main/java/net/quazar/skyengmailingservice/controller/PostalOfficeController.java
@@ -1,8 +1,13 @@
 package net.quazar.skyengmailingservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.quazar.skyengmailingservice.controller.model.CreatePostalOfficeRequest;
+import net.quazar.skyengmailingservice.controller.model.ServerError;
 import net.quazar.skyengmailingservice.entity.dto.PostalOfficeDto;
 import net.quazar.skyengmailingservice.service.PostalOfficeService;
 import org.springframework.web.bind.annotation.*;
@@ -15,26 +20,71 @@ import java.util.Set;
 public class PostalOfficeController {
     private final PostalOfficeService postalOfficeService;
 
+    @Operation(
+            description = "Получает почтовое отделение по его индексу",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = ServerError.class
+                                    )
+                            )
+                    )
+            }
+    )
     @GetMapping("/{index}")
     public PostalOfficeDto getByIndex(@PathVariable String index) {
         return postalOfficeService.findByIndex(index);
     }
 
+    @Operation(
+            description = "Получает список всех почтовых отделений"
+    )
     @GetMapping
     public Set<PostalOfficeDto> getAll() {
         return postalOfficeService.findAll();
     }
 
+    @Operation(
+            description = "Создаёт новое почтовое отделение"
+    )
     @PostMapping
     public PostalOfficeDto createPostalOffice(@Valid @RequestBody CreatePostalOfficeRequest request) {
         return postalOfficeService.createPostalOffice(request.getIndex(), request.getAddress(), request.getName());
     }
 
+    @Operation(
+            description = "Регистрирует почтовое отправление в указанном почтовом отделении",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = ServerError.class
+                                    )
+                            )
+                    )
+            }
+    )
     @PostMapping("/{index}/incoming/{mailingId}")
     public void registerIncomingMailing(@PathVariable String index, @PathVariable int mailingId) {
         postalOfficeService.registerIncomingMailing(index, mailingId);
     }
 
+    @Operation(
+            description = "Регистрирует отправку почтового отправления из почтового отделения",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "404",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = ServerError.class
+                                    )
+                            )
+                    )
+            }
+    )
     @PostMapping("/{index}/outgoing/{mailingId}")
     public void registerOutgoingMailing(@PathVariable String index, @PathVariable int mailingId) {
         postalOfficeService.registerOutgoingMailing(index, mailingId);
